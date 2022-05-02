87a37eda62f18fc86d820885bc6e1f2f95872f34
Lucas F. Voges <lucas.voges@chemie.uni-hamburg.de>
2022-05-02 19:51:47 +0200

    Update the shifted square root reg

--- R/Regression.R
+++ R/Regression.R
@@ -6,22 +6,38 @@
 #' @importFrom caret trainControl train
 #' @import investr
 #' @param CqType this is the Cq value type that should be used.
+#' @param linSqrtTrans Will transform the values to linearise the values! this is basically a shifted square root representation. The parameter fit should be linar as well.
 #' @param fit model for lm() function. "linear", "poly3", "poly4".
 #' @param method method for generating the delta Cq values (see delta.Cq.data())
 #' @param cv should a cross validation be made? With caret!
 #' @param cv.seed seed for cv
 #' @param cv.method The method for cv from caret package
 #' @param cv.p percentage of training data (0 to 1)
-#' @param plot plot the data with plotfit() method from investR - std setting (for more options use plotfit() separately)
+#' @param cvComplete should a cross validation be made with omitting one concentration completely? This will run separately from caret cv.
+#' @param plot plot the data with plotfit() method from investR with std. settings (for more options use plotfit() separately)
+#' @param cv.plot plot the data with plotfit() for the cvComplete cross validation.
 #' @return returns a model: model.delta.Cq object in global space.
 #' @export
-regression.delta.Cq <- function(CqType = "SD", method = "c", fit = "linear", rawPolynomials = FALSE, cv = FALSE, cv.seed = sample(1:100, 1), cv.method = "LGOCV", cv.p = 0.5, plot = TRUE){
+regression.delta.Cq <- function(CqType = "SD", linSqrtTrans = FALSE, method = "c", fit = "linear", rawPolynomials = FALSE, cv = FALSE, cv.seed = sample(1:100, 1), cv.method = "LGOCV", cv.p = 0.5, cvComplete = FALSE, plot = TRUE, cv.plot = FALSE){
 
   if(!exists("data.Cq")){ # Check if delta.Cq is available
     stop("No data.cq list available - please run make.Cq.data()")
   }
 
   modelData.delta.Cq <<- delta.Cq.data(CqType = CqType, method = method, onlyNumeric = TRUE, return = TRUE)
+
+  # SHIFTED SQUARE ROOT TRANSFORMATION
+  if (linSqrtTrans) {
+    #copy data
+    datapoints.sqrt <- modelData.delta.Cq
+    # Make the opposite concentration (offtarget)
+    datapoints.sqrt$oppConcentration <- 100 - datapoints.sqrt$sample
+    # set target concentration -50 and root:
+    datapoints.sqrt$sqrtconc[datapoints.sqrt$sample < 50] <- sqrt(datapoints.sqrt$sample[datapoints.sqrt$sample < 50])-sqrt(50)
+    datapoints.sqrt$sqrtconc[datapoints.sqrt$sample > 50] <- sqrt(datapoints.sqrt$oppConcentration[datapoints.sqrt$sample > 50])
+    datapoints.sqrt$sqrtconc[datapoints.sqrt$sample > 50] <- sqrt(50)-datapoints.sqrt$sqrtconc[datapoints.sqrt$sample > 50]
+    modelData.delta.Cq <<- data.frame(sample = datapoints.sqrt$sqrtconc, delta.Cq = datapoints.sqrt$delta.Cq)
+  }
 
   method <- tolower(gsub(" ","", fit)) #just remove white spaces and lower case
   if (startsWith(method, "l")) {
@@ -50,13 +66,54 @@ regression.delta.Cq <- function(CqType = "SD", method = "c", fit = "linear", raw
   }
 
   if (plot) {
-  plotFit(model.delta.Cq, interval = "both",
+    if (linSqrtTrans) {xl <- "Shifted square root of target proportion"} else {xl <- "Proportion of target genotype [%]"}
+    plotFit(model.delta.Cq, interval = "both",
           level = 0.95, shade = TRUE,
           col.pred = "whitesmoke",
           col.conf = "skyblue",
           extend.range = TRUE,
-          xlab = "Proportion of target genotype [%]",
+          xlab = xl,
           ylab = bquote(Delta*Cq))
+  }
+
+  if (cvComplete){
+
+    # Template for the resulting fataframe
+    test.df <- data.frame(conc=numeric(), estimate.percentage=numeric(), low.dev=numeric(), up.dev=numeric(), error = numeric())
+
+    for (cvc in unique(modelData.delta.Cq$sample)) {
+        # modify data (leave one group out and sets its mean as test data.)
+        data.cvc <- modelData.delta.Cq[modelData.delta.Cq$sample != cvc,]
+        data.cvc.testCq <- mean(modelData.delta.Cq$delta.Cq[modelData.delta.Cq$sample == cvc])
+        # Make a new model for this set
+        model.delta.Cq.cvc <- lm(data = data.cvc, func)
+
+        res.cvc <- invest(model.delta.Cq.cvc, y0 = data.cvc.testCq, interval = "inversion", level = 0.95, mean.response = TRUE, upper = 200, lower = -200)
+
+        if (cv.plot) {
+        plotFit(model.delta.Cq.cvc, interval = "both",
+                level = 0.95, shade = TRUE,
+                col.pred = "whitesmoke",
+                col.conf = "skyblue",
+                extend.range = TRUE,
+                xlab = "Percentage of target genotype [%]",
+                ylab = bquote(Delta~Cq))
+        abline(h = data.cvc.testCq, v = c(res.cvc$lower, res.cvc$estimate, res.cvc$upper), lty = 2, lwd = 0.5, col = "salmon")
+        }
+
+        test.df <- rbind(test.df, data.frame(conc=cvc, estimate.percentage=res.cvc$estimate, low.dev=res.cvc$estimate-res.cvc$lower, up.dev=res.cvc$upper-res.cvc$estimate))
+    }
+    # if squarte root trans: make the sqrt to normal percentage:
+    if (linSqrtTrans){
+      test.df$conc <- abs(((1+sign(test.df$conc))/2) * 100 - (test.df$conc - sign(test.df$conc) * sqrt(50)) ^ 2)
+      test.df$estimate.percentage <- abs(((1+sign(test.df$estimate.percentage))/2) * 100 - (test.df$estimate.percentage - sign(test.df$estimate.percentage) * sqrt(50)) ^ 2)
+    }
+
+    # report the results
+    test.df$error <- abs(test.df$estimate.percentage - test.df$conc)
+    writeLines("\n=====\nLeave one proportion completely out - mean cross validation error [percentage points]:")
+    print(mean(test.df$error))
+    model.complete.cv <<-test.df
   }
 
 }
